e5010ca65fb97b998409b77b14a433fbb39d8e61
sylvainrabette <34077389+sylvainrabette@users.noreply.github.com>
2018-12-09 23:36:46 +0100

    router

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,21 @@
 import React, { Component } from 'react'
+import { HashRouter as Router, Switch, Route, browserHistory } from 'react-router-dom'
 
-import MoviesHelper from '../data/moviesHelper'
-import Constants from '../data/constants'
-import '../styles/MediaQueries.css'
-import '../styles/App.css'
-
-import Header from './Header'
-import MovieTile from './MovieTile'
-import NotFound from './NotFound'
+import MainPage from './MainPage'
 
 class App extends Component {
   constructor () {
     super(...arguments)
-    this.state = {
-      movies: MoviesHelper.getMovies(),
-      searchText: '',
-      category: Constants.Category.All
-    }
-  } 
+  }
 
   render() {
     return (
-      <div className="App">
-        <Header
-          onLiveChange={ (e) => this.onLiveChange(e) }
-          onCategorySelected={ (category) => this.setState({ category })}/>
-        <div className="movie-list">
-          {this.generateMovieTiles()}
-        </div>
-      </div>
+      <Router hashType="noslash">
+        <Switch>
+            <Route path="/" exact={ true } component={MainPage}/>
+        </Switch>
+      </Router>
     )
-  }
-
-  generateMovieTiles() {
-    const movies = MoviesHelper.filter({
-      text: this.state.searchText,
-      category: this.state.category
-    })
-
-    if (movies.length) {
-      return movies.map(movie =>
-        <MovieTile
-          title={movie.title}
-          image={MoviesHelper.getMovieImage(movie)} />
-      )
-    }
-
-    return <NotFound />
-    
-  }
-
-  onLiveChange (e) {
-   const searchText = e.target.value
-   this.setState({ searchText })
   }
 
 }
